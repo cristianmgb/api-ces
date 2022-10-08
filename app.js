f5bca6112d5111855419e5ce463da39f276934dd
Cristian Gonzalez <cristian@bellpi.com>
2022-10-08 11:30:23 -0500

    ajustes de backend

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const app = require("./src/");
 const server = http.createServer(app);
 // const { port } = require('./src/utils/config')
 
-server.listen(5000, () => {
-  console.log(`run desde el puerto 5000`);
+server.listen(5001, () => {
+  console.log(`run desde el puerto 5001`);
 });
--- src/database/index.js
+++ src/database/index.js
@@ -3,10 +3,10 @@ var mysql = require("promise-mysql");
 function query(query) {
   return mysql
     .createConnection({
-      host: "localhost",
-      user: "root",
-      password: "",
-      database: "CES",
+      host: "sql804.main-hosting.eu",
+      user: "u128616487_ces_user",
+      password: "xcdAW#567bj%",
+      database: "u128616487_ces",
       charset: "utf8mb4",
     })
     .then(function (conn) {
